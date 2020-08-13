034e56bdbc36f110bab22902d5d259e2f1a2f6f1
vitek-karas <vitek.karas@microsoft.com>
2020-08-13 06:31:03 -0700

    Simply ALC unload tests

---
+++ FeatureTest/CollectibleALC.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Loader;
+using System.Threading;
+using UtilitiesLibrary;
+
+namespace FeatureTest
+{
+    public class CollectibleALC
+    {
+        [Fact]
+        public void LoadAppAssemblyAndUnload() => ValidateLoadIntoALCAndUnload(nameof(LoadAppAssemblyAndUnload), "FeatureTest");
+
+        [Fact]
+        public void LoadReferencesAssemblyAndUnload() => ValidateLoadIntoALCAndUnload(nameof(LoadAppAssemblyAndUnload), "UtilitiesLibrary");
+
+        [Fact]
+        public void LoadFrameworkAssemblyFromPathAndUnload()
+        {
+            var assembly = typeof(System.Xml.XmlReader).Assembly;
+            string fullPath = DeploymentUtilities.IsSelfContained
+                ? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, assembly.GetName().Name + ".dll")
+                : assembly.Location;
+
+            // BUG https://github.com/dotnet/runtime/issues/40138
+            //if (DeploymentUtilities.IsSingleFile && DeploymentUtilities.IsSelfContained)
+            //{
+            //    Assert.Throws<FileNotFoundException>(() => Assembly.LoadFrom(fullPath));
+            //}
+            //else
+            {
+                ValidateLoadFromPathIntoALCAndUnload(nameof(LoadFrameworkAssemblyFromPathAndUnload), fullPath);
+            }
+        }
+
+        [Fact]
+        public void LoadExternalAssemblyFromPathAndUnload() =>
+            ValidateLoadFromPathIntoALCAndUnload(nameof(LoadExternalAssemblyFromPathAndUnload), Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "PluginLibrary.dll"));
+
+        private void ValidateLoadIntoALCAndUnload(string alcName, string assemblyName)
+        {
+            var alcReference = LoadIntoALCAndUnload(alcName, assemblyName);
+            while (alcReference.IsAlive)
+            {
+                GC.Collect(2, GCCollectionMode.Forced, true);
+                Thread.Sleep(100);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private WeakReference LoadIntoALCAndUnload (string alcName, string assemblyName)
+        {
+            AssemblyLoadContext ctx = new AssemblyLoadContext(nameof(LoadAppAssemblyAndUnload), isCollectible: true);
+            Assembly asm = ctx.LoadFromAssemblyName(new AssemblyName("FeatureTest"));
+            ctx.Unload();
+            return new WeakReference(ctx);
+        }
+
+        private void ValidateLoadFromPathIntoALCAndUnload(string alcName, string assemblyPath)
+        {
+            var alcReference = LoadFromPathIntoALCAndUnload(alcName, assemblyPath);
+            while (alcReference.IsAlive)
+            {
+                GC.Collect(2, GCCollectionMode.Forced, true);
+                Thread.Sleep(100);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private WeakReference LoadFromPathIntoALCAndUnload(string alcName, string assemblyPath)
+        {
+            AssemblyLoadContext ctx = new AssemblyLoadContext(nameof(LoadAppAssemblyAndUnload), isCollectible: true);
+            Assembly asm = ctx.LoadFromAssemblyPath(assemblyPath);
+            ctx.Unload();
+            return new WeakReference(ctx);
+        }
+    }
+}

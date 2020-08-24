17d579bff6d67d11e8391dc7df120021dd3e85ff
vitek-karas <vitek.karas@microsoft.com>
2020-08-24 08:00:23 -0700

    Add test for runtime properties

--- FeatureTest/DependencyResolver.cs
+++ FeatureTest/DependencyResolver.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Reflection;
 using System.Runtime.Loader;
-using UtilitiesLibrary;
 using Xunit;
 
 namespace FeatureTest
---
+++ FeatureTest/RuntimeProperties.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using UtilitiesLibrary;
+using Xunit;
+
+namespace FeatureTest
+{
+    public class RuntimeProperties
+    {
+        [Fact]
+        public void AppContextDepsFiles()
+        {
+            string appContextDepsFiles = (string)AppContext.GetData("APP_CONTEXT_DEPS_FILES");
+            if (DeploymentUtilities.IsSingleFile)
+            {
+                Assert.Null(appContextDepsFiles);
+            }
+            else
+            {
+                string expectedPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "FeatureTest.deps.json");
+                Assert.Contains(expectedPath, appContextDepsFiles);
+            }
+        }
+    }
+}

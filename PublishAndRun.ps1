e32c2807ef618967c19c27d56ca14bfaeae083b0
vitek-karas <vitek.karas@microsoft.com>
2020-08-26 07:58:00 -0700

    Add a script to help run the tests

---
+++ PublishAndRun.ps1
@@ -0,0 +1,58 @@
+[CmdletBinding()]
+param (
+    [switch] $scd,
+    [switch] $sf,
+    [string] $RuntimeRepoLocation
+)
+
+if ([System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::Windows)) {
+    $rid = "win-x64"
+    $coreClrConfig = "Windows_NT.x64.Debug"
+    $coreHostConfig = "win-x64.Debug"
+} elseif ([System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::Linux)) {
+    $rid = "linux-x64"
+    $coreClrConfig = "Linux.x64.Debug"
+    $coreHostConfig = "linux-x64.Debug"
+} else {
+    $rid = "osx"
+}
+
+$args = @()
+$outPath = "$PSScriptRoot/FeatureTest/bin/"
+$args += "publish"
+$args += "$PSScriptRoot/FeatureTest/FeatureTest.csproj"
+
+if ($sf) {
+    $args += "/p:PublishSingleFile=true"
+    $outPath += "sf-"
+}
+else {
+    $outPath += "mf-"
+}
+
+if ($scd) {
+    $args += "--self-contained:true"
+    $args += "-r:$rid"
+    $outPath += "scd"
+}
+else {
+    $args += "--self-contained:false"
+    $outPath += "fdd"
+}
+
+$args += "-o"
+$args += $outPath
+
+if (-Not [string]::IsNullOrEmpty($RuntimeRepoLocation))
+{
+    $args += "/p:CustomizePublishSingleFile_RuntimeRepoRoot=$RuntimeRepoLocation"
+    $args += "/p:CustomizePublishSingleFile_CoreClrConfig=$coreClrConfig"
+    $args += "/p:CustomizePublishSingleFile_CoreHostConfig=$coreHostConfig"
+}
+
+Write-Host "dotnet" $args
+& "dotnet" $args
+
+Write-Host " "
+Write-Host $outPath/FeatureTest
+& $outPath/FeatureTest

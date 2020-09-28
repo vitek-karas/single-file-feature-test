76ec34949640398c474cc3f70cf222a3356cbb9c
vitek-karas <vitek.karas@microsoft.com>
2020-09-28 03:05:49 -0700

    Better test for deps files

--- FeatureTest/RuntimeProperties.cs
+++ FeatureTest/RuntimeProperties.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using UtilitiesLibrary;
 using Xunit;
+using Xunit.Sdk;
 
 namespace FeatureTest
 {
@@ -12,13 +13,21 @@ namespace FeatureTest
         {
             string appContextDepsFiles = (string)AppContext.GetData("APP_CONTEXT_DEPS_FILES");
             string expectedPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "FeatureTest.deps.json");
-            if (DeploymentUtilities.IsSingleFile)
+            if (DeploymentUtilities.IsAssemblyInSingleFile(typeof(RuntimeProperties).Assembly.GetName().Name))
             {
                 Assert.DoesNotContain(expectedPath, appContextDepsFiles);
             }
             else
             {
                 Assert.Contains(expectedPath, appContextDepsFiles);
+            }
+
+            foreach (string path in appContextDepsFiles.Split(Path.PathSeparator, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+            {
+                if (!File.Exists(path))
+                {
+                    throw new XunitException($"APP_CONTEXT_DEPS_FILES contains paths '{path}' which doesn't exist.");
+                }
             }
         }
     }
